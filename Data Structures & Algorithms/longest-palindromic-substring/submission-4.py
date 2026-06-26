bbb60553d86c8fd822e65e7c3f39bc2a8032d0a3
Keane Ferdinand <keaneferdinand82@gmail.com>
2026-06-25 21:24:22 -0400

    Add: longest-palindromic-substring - submission-4

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+    # start at each character and spread left and right. store a local max and return 
+        longest = ""
+        n = len(s)
+        for i in range(0,n): # starting from every charcter 
+            palindrome = s[i] # build our word from this
+            left = i-1
+            right = i+1
+            while left > -1 and right < n and s[right] == s[left]:
+                palindrome = s[right] + palindrome + s[right]
+                left-=1 
+                right+=1 
+            if len(palindrome) > len(longest):
+                longest = palindrome 
+
+        for i in range(0, n-1):
+            palindrome = s[i] + s[i+1]
+            if s[i] != s[i+1]:
+                continue
+            left = i-1
+            right = i+2
+
+            while left > -1 and right < n and s[right] == s[left]:
+                palindrome = s[right] + palindrome + s[right]
+                left-=1 
+                right+=1 
+            if len(palindrome) > len(longest):
+                longest = palindrome
+
+
+
+        return longest
+
