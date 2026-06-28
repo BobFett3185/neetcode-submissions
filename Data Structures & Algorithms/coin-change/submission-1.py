f3963202f7cb795dec61eed27e7de4d4964f29b0
Keane Ferdinand <keaneferdinand82@gmail.com>
2026-06-28 18:32:55 -0400

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # here we can do bottom up tabulation 
+
+        dp =[amount +1] *(amount+1)
+        dp[0] = 0
+
+        for a in range(1, amount+1):
+            for c in coins:
+                if a-c >= 0:
+                    dp[a] = min(dp[a], 1+dp[a-c]) 
+                # if there is an existing remainder 
+                #num of coins plus this 1 coin and its less than current amount of coins 
+                    
+        if dp[amount] == amount+1:
+            return -1
+        return dp[amount]
+
