d1a10f27d4cdf1bc80532dde4d8c95da5960b11e
Keane Ferdinand <keaneferdinand82@gmail.com>
2026-07-03 12:43:09 -0400

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]: # if interval comes before our current spot
+                res.append(newInterval) # then we can append it and add rest of intervals and return
+                return res+intervals[i:]
+            elif newInterval[0] > intervals[i][1]: # if our interval comes after the current spot
+                res.append(intervals[i]) # add current spot and move to next round 
+            else: # we must have a merge so we need to change our new interval and then add it
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+
+        res.append(newInterval)  
+        return res
+
