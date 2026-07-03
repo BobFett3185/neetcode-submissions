e0a29e12e3f59cdce6174de711e8230eec540826
Keane Ferdinand <keaneferdinand82@gmail.com>
2026-07-03 11:42:45 -0400

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # basically work from the end and keep shifing our goalpost towards the front 
+        # start at end and look if the one in front can reach it, if it does 
+        # then we can move our end goalpost to there -- bc we know we can get from there to last index
+
+        # greedy solution working from the back 
+        goal = len(nums) -1
+        for i in range(len(nums)-1, -1, -1): # go from end to front 
+            if nums[i] + i >= goal:
+                goal = i
+        if goal ==0:
+            return True 
+        return False
