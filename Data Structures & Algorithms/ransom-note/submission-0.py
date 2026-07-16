933cc7b127d1ba42637581ef68ca328919bca721
Keane Ferdinand <keaneferdinand82@gmail.com>
2026-07-15 21:01:51 -0400

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # build a hash set where key is the letter and value is the count 
+        counts = {}
+        for letter in magazine:
+            if letter in counts:
+                counts[letter]+=1
+            else:
+                counts[letter] =1
+        
+        for letter in ransomNote:
+            if letter in counts:
+                if counts[letter]==1:
+                    del counts[letter]
+                else:
+                    counts[letter]-=1
+            else:
+                return False
+        return True
+
+        
