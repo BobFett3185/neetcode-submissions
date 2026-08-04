fecf48c9e7163854b9620364b9495821cc438506
Keane Ferdinand <keaneferdinand82@gmail.com>
2026-08-04 18:05:00 -0500

    Add: binary-tree-right-side-view - submission-4

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-4.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        if root == None:
+            return []
+        queue = deque([root])
+        
+        while queue:
+            result.append(queue[-1].val)# add the rightmost element of that level
+
+            for i in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return result 
+
+
+
+'''
+we can only return one element per level 
+so do some kind of breadth first search i think and at each level, add the last element in the queue to the result
+'''
+
+
