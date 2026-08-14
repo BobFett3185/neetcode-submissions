8c2b2c99982e9166af7987e4d0cd9f5f7ec82359
Keane Ferdinand <keaneferdinand82@gmail.com>
2026-08-14 17:46:22 -0500

    Add: subarray-sum-equals-k - submission-2

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        seen = defaultdict(int)
+        prefixSums=[0]*(len(nums)+1)
+
+        # make prefix sums and add them to our seen map
+        for i in range(len(nums)):
+            count = i+1
+            prefixSums[count] = prefixSums[i]+nums[i]
+        
+        print(prefixSums)
+
+        # now go through prefix sums 
+        result =0 
+        for n in prefixSums:
+            
+            result+=seen[n-k]
+            seen[n]+=1
+        return result
+
+
