49aa31a2b0106db7629f38d0cb33e529cff59907
Keane Ferdinand <keaneferdinand82@gmail.com>
2026-09-16 15:23:07 -0500

    Add: maximum-sum-circular-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        # kadanes algorithm 
+        # useful for finding subarray sum maximum 
+
+        #we find the sum of the subarray ending at that index 
+        totalSum = sum(nums)
+        result =nums[0] 
+        currSum =nums[0]
+        for num in nums[1:]:
+            if num > currSum+num: # if number greater than adding num 
+                currSum = num # replace sum
+            else:
+                currSum+=num
+            result = max(result, currSum) # update result
+            
+        print(result)
+
+        # do opposite version
+        currSum2 = nums[0]
+        result2 = nums[0]
+
+        for num in nums[1:]:
+            if num < currSum2 + num:
+                currSum2 = num
+            else:
+                currSum2 = currSum2+num
+            result2 = min(result2, currSum2)
+
+        if result2 == totalSum:
+            return result
+
+        result2 = totalSum-result2
+        
+        return max(result, result2)
+            
+        
